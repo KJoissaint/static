a1b36b2648e4c72485bb8947300a794863f86d46
Karl <joissaintkarl@gmail.com>
2025-06-02 00:59:00 +0200

    minor changes to the navbar

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,19 +4,20 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-gradient-to-r from-[#357C8A] to-[#4CA1AF] shadow sticky top-0 z-50">
+    <header className="bg-gradient-to-r from-[#ffffff] to-[#4CA1AF] shadow sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
-        {/* <Image
+          <div className='flex space-x-4'>
+          <Image
               src="/logo.png"     
               alt="Calmnest Logo"
               width={32}
               height={32}
               className="rounded" 
-            /> */} 
-            {/*I need to fix this later*/}
-
-          <div className="text-2xl font-bold text-yellow-300">Calmnest</div>
+            /> 
+            <Link href="/" className="text-2xl font-bold text-yellow-300">Calmnest</Link>
+          </div>
+            
           <nav className="space-x-6">
             <Link href="/" className="hover:text-yellow-300 text-yellow-300">Accueil</Link>
             <Link href="/contact" className="hover:text-yellow-300 text-yellow-300">Contactez Nous</Link>
